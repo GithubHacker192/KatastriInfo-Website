633e508c0a1309a182530fc80fa67af0bf9a5827
Märt Mattias Alas <marttest9@gmail.com>
2024-10-14 15:37:44 +0300

    Changed request.js

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,30 +1,19 @@
+
 function scrape(code) {
   if (code.search("^[0-9]{5}:[0-9]{3}:[0-9]{4}$") == -1) {
     document.querySelector(".error").style.display = "block";
   } else {
     document.querySelector(".error").style.display = "none";
-    let headers = {
-      accept: "*/*",
-      "accept-encoding": "gzip, deflate, br, zstd",
-      "accept-language": "en-US,en;q=0.9",
-      origin: "https://ky.kataster.ee",
-      priority: "u=1, i",
-      referer: "https://ky.kataster.ee/",
-      "sec-ch-ua":
-        '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site",
-      "user-agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-    };
+    const form = new FormData();
+    form.append("codes", [code]);
     let options = {
-      method: "GET",
-      headers: headers,
+      method: "POST",
+      headers: {
+        'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001',
+      }
     };
-    fetch("https://cadastre.kataster.ee/api/cadastre/" + code, options)
+    options.body = form;
+    fetch("https://web-production-c3f04.up.railway.app/api", options)
       .then((response) => response.json())
       .then((data) => console.log(data));
   }
